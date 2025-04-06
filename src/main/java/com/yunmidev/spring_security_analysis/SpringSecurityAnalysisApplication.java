a68fc6536563a4978e0c48747cdd444e22cc3a89
Jeong-Yunmi <yunmiii0226@khu.ac.kr>
2025-04-06 20:30:56 +0900

    Spring Boot project Initial Setting

---
+++ src/main/java/com/yunmidev/spring_security_analysis/SpringSecurityAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.yunmidev.spring_security_analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAnalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yunmidev/spring_security_analysis/SpringSecurityAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunmidev.spring_security_analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
